eb74ca49fe23fbb5a4964e95783828318835a2bd
TaniaTenorio <taniatenorio@hotmail.com>
2023-06-02 19:50:22 -0600

    feat: add form validation with formik and yup

--- pages/createaccount.js
+++ pages/createaccount.js
@@ -1,13 +1,52 @@
-import Reac from "react"
+import React from "react"
 import Layout from "@/components/Layout"
+import { useFormik } from "formik"
+import * as Yup from 'yup'
 
 const NewAccount = () => {
+  const fieldErrorMsg = "This field is mandatory"
+  const emailErrMsd = 'Invalid Email'
+  const passwordErrMsg = 'Password must have at least 6 characters'
+
+  // Form validation
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required(fieldErrorMsg),
+      lastName: Yup.string().required(fieldErrorMsg),
+      email: Yup.string().email(emailErrMsd).required(fieldErrorMsg),
+      password: Yup.string().required(fieldErrorMsg).min(6, passwordErrMsg)
+    }),
+    onSubmit: (values) => {
+      console.log("enviando", values);
+    },
+  });
+
+  const renderError = (value) => {
+    return(
+      formik.touched[value] && formik.errors[value] ? (
+        <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+          <p className="font-bold">Error</p>
+          <p>{formik.errors[value]}</p>
+        </div>
+      ) : null
+    )
+  }
+
   return (
     <>
       <Layout>
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
-            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+            <form
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -20,23 +59,31 @@ const NewAccount = () => {
                   id="name"
                   type="text"
                   placeholder="User Name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {renderError("name")}
 
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="last-name"
+                  htmlFor="lastName"
                 >
                   Last Name
                 </label>
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="last-name"
+                  id="lastName"
                   type="text"
-                  placeholder="User Last Name"
+                  placeholder="User Name"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {renderError("lastName")}
 
               <div className="mb-4">
                 <label
@@ -50,8 +97,12 @@ const NewAccount = () => {
                   id="email"
                   type="email"
                   placeholder="User Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {renderError("email")}
 
               <div className="mb-4">
                 <label
@@ -65,8 +116,12 @@ const NewAccount = () => {
                   id="password"
                   type="password"
                   placeholder="User Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {renderError("password")}
 
               <input
                 className="bg-gray-800 w-full mt-5 p-2 text-white hover:bg-gray-900"
