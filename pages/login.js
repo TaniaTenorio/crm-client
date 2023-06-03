4a692c3414e16f6136bab59ce0fff25fa1243e32
TaniaTenorio <taniatenorio@hotmail.com>
2023-06-02 18:34:36 -0600

    feat: create login and new account pages

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Reac from "react"
+import Layout from "@/components/Layout"
+
+const Login = () => {
+  return(
+    <>
+      <Layout>
+        <h1 className="text-center text-2xl font-light">Login</h1>
+      </Layout>
+    </>
+  )
+}
+
+export default Login
