7c79c32ce0461e7bb0b87c9e11fb635ccfb6f56c
TaniaTenorio <taniatenorio@hotmail.com>
2023-06-03 21:22:41 -0600

    feat: user hook to protect routes

---
+++ helpers/useUser.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import Router from "next/router";
+import { GET_USER } from './queries'
+import { useQuery } from '@apollo/client'
+
+export default function useUser({
+  redirectTo = '',
+}) {
+  const { data, loading, client } = useQuery(GET_USER)
+  console.log('Loading', loading)
+  
+  React.useEffect(() => {
+    if(!redirectTo || loading) return
+
+    if(redirectTo && !data?.getUser && !loading) {
+      Router.push(redirectTo)
+    }
+  }, [redirectTo, data, loading])
+
+  const user = data?.getUser
+
+  return { user, loading, client } 
+}
