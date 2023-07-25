99010ea4d48af7a4fb0aa4f22da293619ade68ce
TaniaTenorio <taniatenorio@hotmail.com>
2023-07-24 20:34:00 -0600

    create new order page

---
+++ pages/newOrder.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Layout from '@/components/Layout'
+import AssignClient from '@/components/Orders/AssignClient'
+import OrderContext from '@/context/orders/OrderContext'
+
+
+const NewOrder = () => {
+  // Use context y get values and funcs
+  const orderContext = React.useContext(OrderContext)
+
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-200 font-light">New Order</h1>
+      <AssignClient />
+    </Layout>
+
+  );
+}
+
+export default NewOrder
--- pages/orders.js
+++ pages/orders.js
@@ -1,11 +1,18 @@
 import Layout from "@/components/Layout"
-import React from "react"
+import * as React from "react"
+import Link from "next/link";
 
 const Orders = () => {
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-200 font-light">Orders</h1>
+        <Link
+          className="bg-blue-800 py-2 px-5 mt-3 inline-block rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold"
+          href="/newOrder"
+        >
+          New Order
+        </Link>
       </Layout>
     </div>
   );
