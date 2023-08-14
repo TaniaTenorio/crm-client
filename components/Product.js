69f88d315d0d188523fce7a0320460725e8c6fb3
TaniaTenorio <taniatenorio@hotmail.com>
2023-08-14 16:50:47 -0600

    feat: change delete icon

--- components/Product.js
+++ components/Product.js
@@ -61,29 +61,23 @@ const Product = ({product}) => {
       <td className="border px-4 py-2 text-gray-700">{stock}</td>
       <td className="border px-4 py-2 text-gray-700">$ {price}</td>
       <td className="border px-4 py-2 text-gray-700">
-        <button
-          type="button"
-          className="mr-6"
-          onClick={() => handleDelete()}
-        >
+        <button type="button" className="mr-6" onClick={() => handleDelete()}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
+            fill="none"
             viewBox="0 0 24 24"
-            fill="red"
+            strokeWidth={1.5}
+            stroke="red"
             className="w-6 h-6"
           >
             <path
-              fillRule="evenodd"
-              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
-              clipRule="evenodd"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
             />
           </svg>
         </button>
-        <button
-          type="button"
-          className="mr-6"
-          onClick={() => handleEdit()}
-        >
+        <button type="button" className="mr-6" onClick={() => handleEdit()}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
